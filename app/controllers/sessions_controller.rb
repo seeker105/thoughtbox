343b6bf120d704b16a3372b25e4ff04f960c730e
Christopher Soden <karavol27@gmail.com>
2016-09-12 11:41:05 -0600

    Cookie data storage added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: user_params[:email])
     if @user == nil
-      @user = User.new(email: user_params[:email],
-                       password: user_params[:password],
-                       password_confirmation: user_params[:password_confirmation])
-    elsif @user.save
+      @user = User.new(email: user_params[:email], password: user_params[:password], password_confirmation: user_params[:password_confirmation])
+    end
+    if @user.save
       session[:user_id] = @user.id
+      cookies[:user_email] = @user.email
     else
       redirect_to root_path
     end
